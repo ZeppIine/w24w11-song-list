54e56782283a16bfc1f98172f55e6420846bd2bf
ZeppIine <kim0904min@gmail.com>
2024-11-14 14:49:59 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Playlist />
+      <Container />
+    </div>
+  );
+}
+
+function Header() {
+  return <h1>React 프로그래밍</h1>;
+}
+
+function Playlist() {
+  return <div className="playlist">프로그래밍하면서 듣고 싶은 노래</div>;
+}
+
+function Container() {
+  return (
+    <div className="container">
+      <a href="https://www.youtube.com/results?search_query=사랑에 연습이 있었다면">
+        <img src="https://picsum.photos/600/150?random=1" alt="랜덤 이미지 1" />
+        <div class="song-title">사랑에 연습이 있었다면</div>
+      </a>
     </div>
   );
 }
